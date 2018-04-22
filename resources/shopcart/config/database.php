b535a23425955dad0a643d85bf0451faad6fd506
Matthew Foreman <soniceroy@gmail.com>
2018-04-22 13:10:26 -0500

    start changing things...

---
+++ resources/shopcart/config/database.php
@@ -0,0 +1,22 @@
+<?php
+// get a database connection
+class Database {
+    private $host = "localhost";
+    private $db_name = "shop_cart_sessions_1";
+    private $uname = "root";
+    private $pword = "student";
+    public $conn;
+
+    // get database connection
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->uname, $this->pword);
+        }
+        catch(PDOExectoption $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
